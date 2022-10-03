642c40ddf3f5e71aa9f1cb259d634e10029fe599
daniellimcl <113869795+daniellimcl@users.noreply.github.com>
2022-10-04 01:09:29 +0800

    Update Cat.java

--- abstract class/Cat.java
+++ abstract class/Cat.java
@@ -1,7 +1,12 @@
 package Animal;
 
-public class Cat extends Animal {
+public class Cat extends Animal implements Behaviour {
 
+	@Override
+	public void sleep() {
+		System.out.println("zzzzz");
+		
+	}
 	@Override
 	public void makeSound() {
 		System.out.println("meow meow");
