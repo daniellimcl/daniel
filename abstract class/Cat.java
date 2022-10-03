05e88236d4b6dacf886228f2372daff8911b80cf
daniellimcl <113869795+daniellimcl@users.noreply.github.com>
2022-10-04 00:56:39 +0800

    Add files via upload

---
+++ abstract class/Cat.java
@@ -0,0 +1,11 @@
+package Animal;
+
+public class Cat extends Animal {
+
+	@Override
+	public void makeSound() {
+		System.out.println("meow meow");
+		
+	}
+
+}
---
+++ abstract class/absclass.java
@@ -0,0 +1,9 @@
+package Animal;
+
+public class absclass {
+	public static void main (String[]args) {
+		Cat myCat = new Cat();
+		myCat.makeSound();
+	}
+
+}
